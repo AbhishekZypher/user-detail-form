e4acd4b9d6c575ef2294b8ee1802b4247aaf4789
Doctrexx <abhishek123488355@gmail.com>
2023-10-10 16:46:24 -0400

    used Fragments ,Ref and Portals

--- src/Components/UI/ErrorModal/ErrorModal.js
+++ src/Components/UI/ErrorModal/ErrorModal.js
@@ -1,20 +1,41 @@
+import ReactDOM from "react-dom";
+
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 
+const Backdrop = props => {
+    return <div className="fixed top-0 left-0 w-full h-screen z-10 bg-opacity-75 bg-black" onClick={props.onConfirm} />
+}
+const ModalOverlay = props => {
+    return <Card className="fixed top-1/3 left-10 w-80 z-20 overflow-hidden">
+        <header className="bg-purple-600 p-4">
+            <h2 className="text-white">{props.title}</h2>
+        </header>
+        <div className="p-4">
+            <p>{props.message}</p>
+        </div>
+        <footer className="p-4 flex justify-end">
+            <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+    </Card>
+}
+
 const ErrorModal = (props) => {
-    return <div className="fixed top-0 left-0 w-full h-screen z-10 bg-opacity-75 bg-black" onClick={props.onConfirm}>
-        <Card className="fixed top-1/4 left-10 w-80 z-20 overflow-hidden bg-white rounded-lg shadow-lg">
-            <header className="bg-purple-600 p-4">
-                <h2 className="text-white">{props.title}</h2>
-            </header>
-            <div className="p-4">
-                <p>{props.message}</p>
-            </div>
-            <footer className="p-4 flex justify-end">
-                <Button onClick={props.onConfirm}>Okay</Button>
-            </footer>
-        </Card>
-    </div>
+    return <>
+        {ReactDOM.createPortal(
+            <Backdrop onConfirm={props.onConfirm} />,
+            document.getElementById('backdrop-root')
+        )};
+
+        {ReactDOM.createPortal(
+            <ModalOverlay
+                title={props.title}
+                message={props.message}
+                onConfirm={props.onConfirm}
+            />,
+            document.getElementById('overlay-root')
+        )}
+    </>
 };
 
 export default ErrorModal; 
--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import Button from "../UI/Button/Button";
 import ErrorModal from "../UI/ErrorModal/ErrorModal";
 import Card from "../UI/Card/Card";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    // useRef
+    const userNameEntered = useRef();
+    const userAgeEntered = useRef();
 
+
+    // const [enteredUsername, setEnteredUsername] = useState('');
+    // const [enteredAge, setEnteredAge] = useState('');
     const [error, setError] = useState();
 
     const listSubmitted = (event) => {
+
+        const userName = userNameEntered.current.value
+        const userAge = userAgeEntered.current.value
+
         event.preventDefault()
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        if (userName.trim().length === 0 || userAge.trim().length === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter valid name and age, Non empty values'
             });
             return;
         }
-        if (+enteredAge < 18) {
+        if (+userAge < 18) {
             setError({
                 title: 'Invalid Age',
                 message: 'Please enter valid age (>18).'
@@ -27,17 +35,25 @@ const AddUser = (props) => {
             return;
         }
 
-        setEnteredAge('');
-        setEnteredUsername('')
-        props.onAddUser(enteredUsername, enteredAge)
+
+
+        // setEnteredAge('');
+        // setEnteredUsername('')
+
+        // here we are actually setting the html element it's not recommended to manipulate the the html dom but if you just want to set the input values again you can consider doing this, or just use the state method AGAIN! RARELY DO THIS RARELYYYYYYY!!!
+        userNameEntered.current.value = '';
+        userAgeEntered.current.value = '';
+
+        props.onAddUser(userName, userAge)
     }
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    }
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    }
+    // const usernameChangeHandler = (event) => {
+    //     console.log(event.target.value)
+    //     setEnteredUsername(event.target.value);
+    // }
+    // const ageChangeHandler = (event) => {
+    //     setEnteredAge(event.target.value);
+    // }
 
     const errorHandler = () => {
         setError(null);
@@ -50,18 +66,21 @@ const AddUser = (props) => {
                 <form onSubmit={listSubmitted} className="mx-auto my-8 p-4 w-90 max-w-2xl">
                     <label className="block font-bold mb-2">Username</label>
                     <input
-                        value={enteredUsername}
+                        // value={enteredUsername}
+                        autoComplete="off"
                         type="text"
                         className="block w-full border border-gray-300 py-2 px-1 mb-2"
-                        onChange={usernameChangeHandler}
+                        // onChange={usernameChangeHandler}
+                        ref={userNameEntered}
                     />
 
                     <label className="block font-bold mb-2">Age</label>
                     <input
-                        value={enteredAge}
+                        // value={enteredAge}
                         type="number"
                         className="block w-full border border-gray-300 py-2 px-1 mb-2"
-                        onChange={ageChangeHandler}
+                        // onChange={ageChangeHandler}
+                        ref={userAgeEntered}
                     />
                     <Button type="submit">Submit</Button>
                 </form>
